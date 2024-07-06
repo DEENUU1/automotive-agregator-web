e312d26fca313f0062f4de2e9c6fae3c975dd9e1
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-07-06 14:02:45 +0200

    feature: add new models to admin dashboard

--- backend/offers/admin.py
+++ backend/offers/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from offers.models.price import Price
+from offers.models.location import Location
+from offers.models.offer import Offer
 
-# Register your models here.
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('value', 'currency')
+    list_filter = ('currency',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('city', 'country', 'region')
+    list_filter = ('country',)
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'price', 'category')
+    list_filter = ('location', 'category')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at', 'updated_at')
